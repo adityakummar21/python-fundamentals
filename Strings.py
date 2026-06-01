67ed2d948a9bb83695643b679c7bc7d25d56ef7d
Aditya Kumar <adityakummar21@gmail.com>
2026-06-01 14:34:00 -0400

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,52 @@
+#my_str1 = "Hello"
+#my_str2 = "GoodBye"
+
+
+
+#print('ell' in my_str1)
+#print('dont' in my_str2, 'ello' in my_str2, 'ello' in my_str1) # This checks to see if certain letters exist
+
+##Use parantheses to print the length 
+#print(len(my_str1))
+
+
+##Index:
+#In a string, each character is represented by an index value staring at 0 For Example:
+#print(my_str1[2]) #This will print the first l found in hello
+#Negative indexing also exists, which takes you from the right to left. 
+#print(my_str1[-2]) #Which prints the 2nd l found in hello
+
+
+##String Concatenation is the joining of 2 string, Ex:
+#print(my_str1 + '', my_str2)
+
+
+##String interpolation: We can also combne a string with and integer as as whole string
+#name = 'Aditya'
+#age = 25
+
+#name_and_age = name
+
+#name_and_age += str(age)
+
+#print(name_and_age)
+
+
+
+## f strings knows as formated string literals is a built int
+## allows you to handle interpolation with a compact and readable syntax
+
+#name = "Drake"
+#planet_type = 'Venus'
+#favorite_thing = "Penis"
+
+#print(f'My name is {name} and I like {favorite_thing} I come from {planet_type} I a the meanest')
+
+
+
+
+
+
+
+
+
